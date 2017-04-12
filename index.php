331e5aee977185a700d1c77e51531264db1185b8
Aleksandrs <sanjkagm@inbox.lv>
2017-04-12 16:43:11 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>MyProfilePage</title>
+<link href="main.css" rel="stylesheet" type="text/css">
+</head>
+
+	<body>
+    	
+   	<h1>This is my Profile page. Welcome!!!</h1>
+        <table id="tablename">
+        	<tr>
+            	<td>My name is:</td>
+                <td>Aleksandrs</td>
+            </tr>
+            <tr>
+            	<td>What I do:</td>
+                <td>Tryin' to learn how to programm stuff</td>
+            </tr>
+            <tr>
+            	<td>Where you can call?:</td>
+                <td>+371 22254445565</td>
+            </tr>
+            <tr>
+            	<td>Want to send email?:</td>
+                <td>aleksandrs@whatever.com</td>
+            </tr>
+        </table>
+        <p>This is a part where I should tell something about me... <br> 
+        Well, I'm lazy and I don't want to write anything)) Sorry!
+        But what I do like is:</p>
+   	    <ul>
+        		<li>Choclate</li>
+                <li>Sleeping</li>
+                <li>Punching things</li>
+    	</ul>
+        <p>And yes... Here's an example a tag link in this next word. <a href="http://www.facebook.com">Link</a>!</p>
+        <div id="photo">
+   	    	<img src="images/wes.jpg" width="450" height="300" alt=""/> 
+        </div>
+    </body>
+</html>
